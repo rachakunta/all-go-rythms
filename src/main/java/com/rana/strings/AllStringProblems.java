7e28a76fc2d8c741cc38d1c891cf09b6edab64ea
rachakunta <harshayadav05@gmail.com>
2019-08-27 23:52:46 -0500

    word_ladder

--- src/main/java/com/rana/strings/AllStringProblems.java
+++ src/main/java/com/rana/strings/AllStringProblems.java
@@ -1,4 +1,43 @@
 package com.rana.strings;
 
+import java.util.*;
+
 public class AllStringProblems {
+    public static void main(String[] args) {
+        String[] s = {"hot","dot","dog","lot","log","cog"};
+        String begin = "hit", end = "cog";
+        System.out.println(ladderLength(begin, end, Arrays.asList(s)));
+    }
+
+    public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
+        if(!set.contains(endWord)){
+            return 0;
+        }
+        Queue<String> q = new LinkedList<>();
+        q.offer(beginWord);
+        int result = 1;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int k = 0; k< size; k++) {
+                String polled = q.poll();
+                char[] chars = polled.toCharArray();
+                for (int i = 0; i < chars.length; i++) {
+                    char temp = chars[i];
+                    for (char t = 'a'; t < 'z'; t++) {
+                        chars[i] = t;
+                        String s = new String(chars);
+                        if (set.contains(s)) {
+                            if (s.equals(endWord)) return result + 1;
+                            q.add(s);
+                            set.remove(s);
+                        }
+                    }
+                    chars[i] = temp;
+                }
+            }
+            result++;
+        }
+        return 0;
+    }
 }
